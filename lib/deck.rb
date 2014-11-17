e0e4738aac893712695d4f446c87b4d648d6c376
Zachary Perlmutter <zrp200@gmail.com>
2014-11-17 13:02:09 -0800

    Update deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,6 +10,7 @@ class Deck # The 40-card deck used in this game
       stock "Jack", suit
       for num in (2..7).to_a; stock num, suit; end
     end
+  end
   def draw; @cards.shift; end # Draw a card from @cards
-  def stock(card, suit); @cards.push CardDeck::Card.new card, suit; end # Creates a card and puts it into @cards
+  def stock(card, suit); @cards.push CardDeck::Card.new(card, suit); end # Creates a card and puts it into @cards
 end
