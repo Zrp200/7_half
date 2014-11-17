28a54aef0549ba0e8bcfea4e0e22674658d75378
Zachary Perlmutter <zrp200@gmail.com>
2014-11-17 12:45:01 -0800

    Update card.rb

--- lib/card.rb
+++ lib/card.rb
@@ -3,9 +3,10 @@ class CardDeck::Card # Main object used. Makes this a 'Card' game
 	def value
 		case num
 			when "Ace" then 1
-			when 2..7 then num.to_i
-		else
-			0.5
+			when 2..7 then num
+			when "King" then 0.5
+			when "Queen" then 0.5
+			when "Jack" then 0.5
 		end
 	end
 end
