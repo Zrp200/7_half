52dbaf8118fe1c146cb0207ab430f3cb1930836c
Zachary Perlmutter <zrp200@gmail.com>
2014-11-16 22:54:19 -0800

    Update card_spec.rb

--- spec/lib/card_spec.rb
+++ spec/lib/card_spec.rb
@@ -1,3 +1,10 @@
-RSpec.describe CardDeck::Card.new do
-  it "should work"; end
+RSpec.describe CardDeck::Card do
+  describe "#value" do
+    context "when Ace" do
+      subject {CardDeck.Card("Ace", "diamonds").value}
+      it {is_expected.to eq 1}
+    end
+    context "when Integer" do
+      it "should return itself"
+    end
 end
