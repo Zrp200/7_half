1a9e854122fb8a3a10195f8d9469a4372900ca28
Zachary Perlmutter <zrp200@gmail.com>
2014-11-16 23:30:45 -0800

    Update hand.rb

---
+++ lib/hand.rb
@@ -0,0 +1,7 @@
+class Hand # Acts as the player
+  DECK = Deck.new.shuffle # The deck used in the game
+  MDHV = 7.5 * (16.0/21.0) # Max dealer hit value. It is at the same ratio as blackjack.
+  def initialize; end
+end
+  
+  
