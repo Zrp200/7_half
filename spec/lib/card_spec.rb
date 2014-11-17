92e2a2d295d9c4d6b7299f543d8ec5df49c255af
Zachary Perlmutter <zrp200@gmail.com>
2014-11-17 12:26:33 -0800

    Update card_spec.rb

--- spec/lib/card_spec.rb
+++ spec/lib/card_spec.rb
@@ -1,13 +1,15 @@
 require "card"
 RSpec.describe CardDeck::Card do
   describe "#value" do
-    context "when Ace" do
-      subject {CardDeck::Card.new("Ace", "diamonds").value}
-      it {is_expected.to eq 1}
-    end
-    context "when 2" do
-      subject {CardDeck::Card.new(2, 'diamonds').value}
-      it {is_expected.to eq 2}
+    describe "num" do
+      context "when Ace" do
+        subject {CardDeck::Card.new("Ace", "diamonds").value}
+        it {is_expected.to eq 1}
+      end
+      context "when 2" do
+        subject {CardDeck::Card.new(2, 'diamonds').value}
+        it {is_expected.to eq 2}
+      end
     end
   end
 end
