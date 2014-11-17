24562037d289f1b7aaf5dce83c591bfddefef64a
Zachary Perlmutter <zrp200@gmail.com>
2014-11-16 23:39:57 -0800

    Update deck.rb

---
+++ lib/deck.rb
@@ -0,0 +1,16 @@
+require "card_deck"
+require_relative "card.rb"
+class Deck # The 40-card deck used in this game
+  private :stock
+  def initialize
+    @cards = Array.new
+    for suit in CardDeck::Card::SUIT
+      stock "Ace", suit
+      stock "King", suit
+      stock "Queen", suit
+      stock "Jack", suit
+      for num in (2..7).to_a; stock num, suit; end
+    end
+  def draw; @cards.shift; end # Draw a card from @cards
+  def stock(card, suit); @cards.push CardDeck::Card.new card, suit; end # Creates a card and puts it into @cards
+end
