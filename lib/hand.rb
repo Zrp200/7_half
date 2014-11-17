e51a153ca24749ca13f37826fb18bf43cce5a326
Zachary Perlmutter <zrp200@gmail.com>
2014-11-17 12:53:31 -0800

    Update hand.rb

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,3 +1,5 @@
+require_relative "deck"
+require_relative "card"
 class Hand # Acts as the player
   DECK = Deck.new.shuffle # The deck used in the game
   MDHV = 7.5 * (16.0/21.0) # Max dealer hit value. It is at the same ratio as blackjack.
