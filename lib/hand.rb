c00c05c7c75b5512af11b0dcd77160e542e1e7f5
Zachary Perlmutter <zrp200@gmail.com>
2014-11-17 12:54:33 -0800

    Update hand.rb

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,9 +1,9 @@
-require_relative "deck"
-require_relative "card"
+require_relative "deck.rb"
+require_relative "card.rb"
 class Hand # Acts as the player
   DECK = Deck.new.shuffle # The deck used in the game
   MDHV = 7.5 * (16.0/21.0) # Max dealer hit value. It is at the same ratio as blackjack.
-  def initialize; end
+  def initialize; @cards == [DECK.draw]; end
 end
   
   
