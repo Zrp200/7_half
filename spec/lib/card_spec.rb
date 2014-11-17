e6459d6d5ff9e3c388173453451b1451a06facca
Zachary Perlmutter <zrp200@gmail.com>
2014-11-16 23:22:08 -0800

    Update card_spec.rb

--- spec/lib/card_spec.rb
+++ spec/lib/card_spec.rb
@@ -2,7 +2,7 @@ require "card"
 RSpec.describe CardDeck::Card do
   describe "#value" do
     context "when Ace" do
-      subject {CardDeck.Card("Ace", "diamonds").value}
+      subject {CardDeck::Card.new("Ace", "diamonds").value}
       it {is_expected.to eq 1}
     end
     context "when Integer" do
