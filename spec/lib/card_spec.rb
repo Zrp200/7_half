1d7918dd224e1e95bb0370efa9e17565c0e151a1
Zachary Perlmutter <zrp200@gmail.com>
2014-11-17 21:41:39 -0800

    Update card_spec.rb

--- spec/lib/card_spec.rb
+++ spec/lib/card_spec.rb
@@ -6,12 +6,22 @@ RSpec.describe CardDeck::Card do
         subject {CardDeck::Card.new("Ace", "diamonds").value}
         it {is_expected.to eq 1}
       end
-      context "when 2" do
-        subject {CardDeck::Card.new(2, 'diamonds').value}
-        it {is_expected.to eq 2}
+      context "when 2..7" do
+        it "should eq @num" do
+          s = CardDeck::Card.new
+          expect(2..7).to cover(s.value)
+        end
       end
       context "when King" do
-        subject {CardDeck::Card.new "King", 'diamonds'}
+        subject {CardDeck::Card.new("King",'diamonds').value}
+        it {is_expected.to eq 0.5}
+      end
+      context "when Queen" do
+        subject {CardDeck::Card.new ("Queen",'diamonds').value}
+        it {is_expected.to eq 0.5}
+      end
+      context "when Jack" do
+        subject {CardDeck::Card.new ("Jack", 'diamonds').value}
         it {is_expected.to eq 0.5}
       end
     end
