a409d8f449f22c6a916a105e049eae2112dd76b2
Zachary Perlmutter <zrp200@gmail.com>
2014-11-18 12:58:04 -0800

    Update hand.rb

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,8 +1,14 @@
 require_relative "deck.rb" # Load Deck
 class Hand # Acts as the player
+  attr_accessor :cards # The cards in your hand
   DECK = Deck.new # The deck used in the game
-  MDHV = 7.5 * (17.0/21.0) # Minimum value the dealer can stay at. Uses the same ratio as blackjack1.
+  MDSV = 7.5 * (17.0/21.0) # Minimum value the dealer can stay at. Uses the same ratio as blackjack1.
   def initialize; @cards = [DECK.draw]; end
+  def card; @cards.push DECK.draw; end # Adds a card to your hand, increasing your value
+  def value # The combined value of all the cards in your hand
+    sum = 0.0
+    for c in @cards; sum += c.value; end
+  end
 end
   
   
