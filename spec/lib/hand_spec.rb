00acc17709085a29c2f606330aff371c19d7a252
Zachary Perlmutter <zrp200@gmail.com>
2014-11-17 21:47:44 -0800

    Update hand_spec.rb

--- spec/lib/hand_spec.rb
+++ spec/lib/hand_spec.rb
@@ -7,11 +7,12 @@ describe Hand do
     subject {Hand::MDHV}
     it {is_expected.to be < 7.5}
     it {is_expected.to be_instance_of Float}
+    it {is_expected.to_not be_instance_of Integer}
   end
   describe "::DECK" do
     subject {Hand::DECK}
     it {is_expected.to be_instance_of Deck}
     it {is_expected.to_not be_instance_of CardDeck::Deck}
-    its(:sample) {is_expected.to be_instance_of CardDeck::Card}
+    its(:draw) {is_expected.to be_instance_of CardDeck::Card}
   end
 end
