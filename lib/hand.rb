e00aba105f7943afc193c66dfe398203df29cab5
Zachary Perlmutter <zrp200@gmail.com>
2014-11-18 12:45:02 -0800

    Update hand.rb

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,7 +1,7 @@
 require_relative "deck.rb" # Load Deck
 class Hand # Acts as the player
   DECK = Deck.new # The deck used in the game
-  MDHV = 7.5 * (16.0/21.0) # Max dealer hit value. It is at the same ratio as blackjack.
+  MDHV = 7.5 * (17.0/21.0) # Minimum value the dealer can stay at. Uses the same ratio as blackjack1.
   def initialize; @cards = [DECK.draw]; end
 end
   
