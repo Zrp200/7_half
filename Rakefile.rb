80d83989d0b8f8c9a9650bccbc2dbbe0e49e4bf4
Zachary Perlmutter <zrp200@gmail.com>
2014-11-28 11:56:28 -0800

    Update Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,13 +1,1 @@
-require "mail"
-namespace 'travis' do
-  task :email do
-    mail = Mail.new do
-      to 'michael.a.perlmutter.gmail.com'
-      from "zrp200@gmail.com"
-      subject  "Released #{ENV['TRAVIS_TAG']}"
-      body "`gem install 7_half`"
-      body("`gem install 7_half --pre`") if ENV['TRAVIS_TAG'].include?('pre')
-    end
-    mail.deliver
-  end
-end
+
